1f083a300716b668adaf7a90c4c49bd4f785357d
Kiran Asapu <Kiran.Asapu@247-inc.com>
2022-11-26 09:32:05 +0530

    some features

--- complete_react_guide/src/App.js
+++ complete_react_guide/src/App.js
@@ -30,9 +30,14 @@ function App() {
     },
   ];
 
+  const addExpenseHandler = (expense) => {
+    console.log('In App.js');
+    console.log(expense);
+  };
+
   return (
     <div>
-      <NewExpense />
+      <NewExpense onAddExpenseData={addExpenseHandler}/>
       <Expenses items={expenses}/>
     </div>
   );
