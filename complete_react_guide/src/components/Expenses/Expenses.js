255747f4ecc25d8e17d869a65d11dade47756a50
kiran-teja <kirantejaasapu@gmail.com>
2022-11-26 10:49:54 +0530

    Dynamic Expenses List

--- complete_react_guide/src/components/Expenses/Expenses.js
+++ complete_react_guide/src/components/Expenses/Expenses.js
@@ -11,14 +11,16 @@ function Expenses(props) {
         setFilteredYear(selectedYear)
     };
 
+    const filteredExpenses = props.items.filter(item => {
+        return item.date.getFullYear().toString() === filteredYear});
+
     return (
         <div>
             <Card className="expenses">
                 <ExpenseFilter selected={filteredYear} onFilterDateChange = {filterChangeHandler}></ExpenseFilter>
-                <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}></ExpenseItem>
-                <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date}></ExpenseItem>
-                <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date}></ExpenseItem>
-                <ExpenseItem title={props.items[3].title} amount={props.items[3].amount} date={props.items[3].date}></ExpenseItem>
+                {filteredExpenses.map(item => {
+                    return (<ExpenseItem key={item.id} title={item.title} amount={item.amount} date={item.date}/>)
+                })}
             </Card>
         </div>
     );
