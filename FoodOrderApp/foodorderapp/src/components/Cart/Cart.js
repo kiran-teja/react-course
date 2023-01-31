1071757b2faa8a9b1e8b98148a06a09398a03c24
Kiran Asapu <Kiran.Asapu@247-inc.com>
2023-01-31 12:42:41 +0530

    Added context and modal

--- FoodOrderApp/foodorderapp/src/components/Cart/Cart.js
+++ FoodOrderApp/foodorderapp/src/components/Cart/Cart.js
@@ -1,3 +1,4 @@
+import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
 
 const Cart = props => {
@@ -9,17 +10,17 @@ const Cart = props => {
       }
     ].map((item) => <li>{item.name}</li>)}</ul>;
 
-  return <div>
+  return <Modal onClose={props.onClose}>
     {cartItems}
     <div className={classes.total}>
         <span>Total Amount</span>
         <span>35.62</span>
     </div>
     <div className={classes.actions}>
-        <button className={classes['button--alt']}>Close</button>
+        <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
         <button className={classes.button}>Order</button>
     </div>
-  </div>
+  </Modal>
 };
 
 export default Cart;
