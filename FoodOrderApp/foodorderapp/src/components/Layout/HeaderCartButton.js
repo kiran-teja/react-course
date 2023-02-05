7674b06aaf64758d36d2ab58457de22a6003a80c
kiran-teja <kirantejaasapu@gmail.com>
2023-02-05 21:15:55 +0530

    Cart changes

--- FoodOrderApp/foodorderapp/src/components/Layout/HeaderCartButton.js
+++ FoodOrderApp/foodorderapp/src/components/Layout/HeaderCartButton.js
@@ -1,6 +1,15 @@
+import { useContext } from 'react';
+
 import CartIcon from '../Cart/CartIcon';
+import CartContext from '../../store/cart-context';
 import classes from './HeaderCartButton.module.css';
+
 const HeaderCartButton = props => {
+    const cartCtx = useContext(CartContext);
+    const numberOfCartItems = cartCtx.items.reduce((total, item) => {
+        return total+item.amount;
+    }, 0);
+
     return (
         <button className={classes.button} onClick={props.onClick}>
             <span className={classes.icon}>
@@ -10,7 +19,7 @@ const HeaderCartButton = props => {
                 Your Cart
             </span>
             <span className={classes.badge}>
-                3
+                {numberOfCartItems}
             </span>
         </button>
     );
--- FoodOrderApp/foodorderapp/src/components/Meals/MealItem/MealItemForm.js
+++ FoodOrderApp/foodorderapp/src/components/Meals/MealItem/MealItemForm.js
@@ -1,11 +1,27 @@
+import { useRef, useState } from 'react';
 import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css';
 
 const MealItemForm = props => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+  const amountInputRef = useRef();
+  const submitHandler = event => {
+    event.preventDefault();
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+
+    if(enteredAmount.trim().length === 0 || enteredAmountNumber < 1 || enteredAmountNumber > 5){
+      setAmountIsValid(false);
+      return;
+    }
+
+    props.onAddToCart(enteredAmountNumber);
+  }
   return (
-    <form className={classes.form}>
-      <Input label="Amount" input={{id: 'amount', type: 'number', min: '1', max: '5', step: '1', defaultValue: '1'}}/>
+    <form className={classes.form} onSubmit={submitHandler}>
+      <Input label="Amount" input={{ref:amountInputRef, id: 'amount', type: 'number', min: '1', max: '5', step: '1', defaultValue: '1'}}/>
       <button>+ Add</button>
+      {!!amountIsValid && <p>Please enter a valid amount.</p>}
     </form>
   );
 };
